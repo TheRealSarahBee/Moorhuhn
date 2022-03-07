d6c024ecfc3376e157db037e6d93adac59d78a06
sarah <sarah@SARAH-PC>
2022-03-07 17:31:32 +0100

    Projektdateien hinzufügen.

---
+++ Moorhuhn/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace Moorhuhn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timerDelete = new System.Windows.Forms.Timer(this.components);
+            this.lblTreffer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 47);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timerDelete
+            // 
+            this.timerDelete.Enabled = true;
+            this.timerDelete.Interval = 1000;
+            this.timerDelete.Tick += new System.EventHandler(this.timerDelete_Tick);
+            // 
+            // lblTreffer
+            // 
+            this.lblTreffer.AutoSize = true;
+            this.lblTreffer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTreffer.Location = new System.Drawing.Point(118, 12);
+            this.lblTreffer.Name = "lblTreffer";
+            this.lblTreffer.Size = new System.Drawing.Size(109, 25);
+            this.lblTreffer.TabIndex = 1;
+            this.lblTreffer.Text = "Treffer: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTreffer);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timerDelete;
+        private System.Windows.Forms.Label lblTreffer;
+    }
+}
+
---
+++ Moorhuhn/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Moorhuhn
+{
+    public partial class Form1 : Form
+    {
+        
+        List<PictureBox> items = new List<PictureBox>();
+        PictureBox box = new PictureBox();
+        Random rnd = new Random();
+        Boolean start = false;       
+        int counter = 0;
+       
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+            
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            start = true;
+
+        }
+
+        private void MakePictureBox()
+        {
+               
+                box.Width = 50;
+                box.Height = 50;
+                box.BackColor = Color.Red;
+                int x = rnd.Next(10, this.ClientSize.Width - box.Width);
+                int y = rnd.Next(10, this.ClientSize.Height - box.Height);
+                box.Location = new Point(x, y);
+
+                items.Add(box);
+                Controls.Add(box);
+
+            
+                box.Click += new EventHandler(PictureBoxClick);
+
+        }
+
+        private void PictureBoxClick(object sender, EventArgs e)
+        {
+            
+            if (sender is PictureBox)
+            {
+                PictureBox box = (PictureBox)sender;
+                items.Remove(box);
+                Controls.Remove(box);
+                counter++;
+              
+                lblTreffer.Text = "Treffer: " + counter;
+            }
+
+        }
+
+
+        private void timerDelete_Tick(object sender, EventArgs e)
+        {
+            
+            int i = rnd.Next(0, 5);
+
+            if(start == true && i>0 && i<5)
+            {
+                MakePictureBox();
+                
+            }
+
+            
+            
+
+        }
+    }
+}
